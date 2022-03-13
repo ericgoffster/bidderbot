18aee137a1c087cdab3e0d4091ba7ab306daf3d3
Eric Goff <egoff@apple.com>
2022-03-13 11:45:38 -0500

    more basic functionality

---
+++ src/main/java/bbidder/inferences/StoppersInSuits.java
@@ -0,0 +1,71 @@
+package bbidder.inferences;
+
+import java.util.List;
+import java.util.Objects;
+
+import bbidder.BitUtil;
+import bbidder.Inference;
+import bbidder.InferenceContext;
+import bbidder.MappedInference;
+import bbidder.SplitUtil;
+import bbidder.StopperSet;
+import bbidder.inferences.bound.StoppersBoundInf;
+
+/**
+ * Represents the inference of the stoppers in a suit set.
+ * 
+ * @author goffster
+ *
+ */
+public class StoppersInSuits implements Inference {
+    public final String suits;
+
+    public StoppersInSuits(String suits) {
+        super();
+        this.suits = suits;
+    }
+    
+    public static Inference valueOf(String str) {
+        String[] parts = SplitUtil.split(str, "\\s+", 2);
+        if (parts.length == 2 && parts[0].equalsIgnoreCase("stoppers")) {
+            return new StoppersInSuits(parts[1]);
+        }
+        return null;
+    }
+
+    @Override
+    public List<MappedInference> bind(InferenceContext context) {
+        short theSuits = context.lookupSuitSet(suits);
+        StopperSet stoppers = new StopperSet(stopper -> {
+            for(int s: BitUtil.iterate(theSuits)) {
+                if (!stopper.stopperIn(s)) {
+                    return false;
+                }
+            }
+            return true;
+        });
+        return List.of(new MappedInference(StoppersBoundInf.create(stoppers), context));
+    }
+
+    @Override
+    public String toString() {
+        return "stoppers "+ suits;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(suits);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        StoppersInSuits other = (StoppersInSuits) obj;
+        return Objects.equals(suits, other.suits);
+    }
+}
---
+++ src/main/java/bbidder/inferences/bound/StoppersBoundInf.java
@@ -0,0 +1,87 @@
+package bbidder.inferences.bound;
+
+import java.util.Objects;
+
+import bbidder.Hand;
+import bbidder.IBoundInference;
+import bbidder.InfSummary;
+import bbidder.Range;
+import bbidder.ShapeSet;
+import bbidder.StopperSet;
+
+/**
+ * Represents the inference for stoppers.
+ * 
+ * @author goffster
+ *
+ */
+public class StoppersBoundInf implements IBoundInference {
+    final StopperSet stoppers;
+
+    @Override
+    public IBoundInference negate() {
+        return create(stoppers.not());
+    }
+
+    private StoppersBoundInf(StopperSet stoppers) {
+        this.stoppers = stoppers;
+    }
+
+    @Override
+    public InfSummary getSummary() {
+        return new InfSummary(ShapeSet.ALL, Range.all(40), stoppers);
+    }
+
+    @Override
+    public IBoundInference andWith(IBoundInference other) {
+        if (other instanceof StoppersBoundInf) {
+            return create(((StoppersBoundInf) other).stoppers.and(stoppers));
+        }
+        return null;
+    }
+
+    @Override
+    public IBoundInference orWith(IBoundInference other) {
+        if (other instanceof StoppersBoundInf) {
+            return create(((StoppersBoundInf) other).stoppers.or(stoppers));
+        }
+        return null;
+    }
+
+    public static IBoundInference create(StopperSet r) {
+        if (r.isEmpty()) {
+            return ConstBoundInference.F;
+        }
+        if (r.unBounded()) {
+            return ConstBoundInference.T;
+        }
+        return new StoppersBoundInf(r);
+    }
+
+    @Override
+    public boolean matches(Hand hand) {
+        return stoppers.contains(hand.getStoppers());
+    }
+
+    @Override
+    public String toString() {
+        return stoppers.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(stoppers);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        StoppersBoundInf other = (StoppersBoundInf) obj;
+        return Objects.equals(stoppers, other.stoppers);
+    }
+}
