10d902e95ea686226a6722c7491fb5b5ab72cb71
Eric Goff <egoff@apple.com>
2022-03-13 08:54:39 -0500

    Revert "more basic functionality"
    This reverts commit 4160ba5743ba028662e4a28b92a7990dc6cabbba.

--- src/test/java/bbidder/Test2Over1.java
+++ src/test/java/bbidder/Test2Over1.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,9 +14,11 @@ public class Test2Over1 {
 
     @Test
     public void test() throws IOException {
+        List<ParseException> exes = new ArrayList<>();
         BiddingSystem bs = BiddingSystem.load("", "classpath:2over1.bidding", ex -> {
-            ex.printStackTrace();
+            exes.add(ex);
         });
+        assertEquals(List.of(), exes);
         for(BiddingTest test: bs.tests) {
             TestResult result = test.getResult(bs);
             assertEquals(result.hand + ":" + result.bids, result.expected, result.found);
