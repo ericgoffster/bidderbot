52d4a64e3210e4dcf1ce876a1ec64ec7130f2ff1
Eric Goff <egoff@apple.com>
2022-03-13 11:45:38 -0500

    more basic functionality

---
+++ src/main/java/bbidder/Contract.java
@@ -0,0 +1,39 @@
+package bbidder;
+
+import java.util.Objects;
+
+public class Contract {
+    public final int position;
+    public final Bid winningBid;
+    public final boolean doubled;
+    public final boolean redoubled;
+    public Contract(int position, Bid winningBid, boolean doubled, boolean redoubled) {
+        super();
+        this.position = position;
+        this.winningBid = winningBid;
+        this.doubled = doubled;
+        this.redoubled = redoubled;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(doubled, position, redoubled, winningBid);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Contract other = (Contract) obj;
+        return doubled == other.doubled && position == other.position && redoubled == other.redoubled && winningBid == other.winningBid;
+    }
+    @Override
+    public String toString() {
+        if (winningBid == Bid.P) {
+            return "all pass";
+        }
+        return winningBid + (redoubled ? "XX" : doubled ? "X" : "") + " by "+ position;
+    }
+}
