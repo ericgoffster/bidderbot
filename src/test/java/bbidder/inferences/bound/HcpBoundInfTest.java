3bf60a6c8e2b600b618a18f9b2c2d545f0203afc
Eric Goff <egoff@apple.com>
2022-03-13 11:45:38 -0500

    more basic functionality

---
+++ src/test/java/bbidder/inferences/bound/HcpBoundInfTest.java
@@ -0,0 +1,24 @@
+package bbidder.inferences.bound;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import bbidder.Hand;
+import bbidder.InfSummary;
+import bbidder.Range;
+
+public class HcpBoundInfTest {
+    @Test
+    public void test() {
+        assertEquals(HcpBoundInf.create(Range.all(40)), ConstBoundInference.T);
+        assertEquals(HcpBoundInf.create(Range.none(40)), ConstBoundInference.F);
+        assertTrue(HcpBoundInf.create(Range.between(10, 11, 40)).matches(Hand.valueOf("AKxx Kxx xxx xxx")));
+        assertFalse(HcpBoundInf.create(Range.between(10, 11, 40)).matches(Hand.valueOf("AKxx Qxx xxx xxx")));
+        assertTrue(HcpBoundInf.create(Range.between(10, 11, 40)).matches(Hand.valueOf("AKxx KJx xxx xxx")));
+        InfSummary summary = HcpBoundInf.create(Range.between(10, 11, 40)).getSummary();
+        assertEquals(summary, InfSummary.ALL);
+    }
+}
