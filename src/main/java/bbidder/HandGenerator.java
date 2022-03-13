aaab20c2f896f0fc8017adc19e45bd751aa44126
Eric Goff <egoff@apple.com>
2022-03-13 11:45:38 -0500

    more basic functionality

--- src/main/java/bbidder/HandGenerator.java
+++ src/main/java/bbidder/HandGenerator.java
@@ -28,10 +28,7 @@ public class HandGenerator {
                 cards[i] = t;
                 h = h.withCardAdded(t % 4, t / 4);
             }
-            if (summary.shape.contains(h.getShape()) && inf.matches(h)) {
-                if (!summary.hcp.contains(h.numHCP())) {
-                    throw new RuntimeException();
-                }
+            if (summary.shape.contains(h.getShape()) && summary.hcp.contains(h.numHCP()) && inf.matches(h)) {
                 result.add(h);
             }
             num++;
