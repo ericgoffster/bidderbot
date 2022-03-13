8004d55779458407f1a1100351fdaeab2b9c27dc
Eric Goff <egoff@apple.com>
2022-03-13 11:45:38 -0500

    more basic functionality

--- src/main/java/bbidder/HandList.java
+++ src/main/java/bbidder/HandList.java
@@ -8,79 +8,11 @@ import java.util.List;
  * @author goffster
  *
  */
-public class HandList implements IHandList {
+public class HandList {
     public final List<Hand> l;
 
     public HandList(List<Hand> l) {
         super();
         this.l = l;
-    }
-
-    public int minTotalPoints(int suit) {
-        Integer minHcp = null;
-        for (Hand h : l) {
-            int hcp = h.totalPoints(suit);
-            if (minHcp == null || hcp < minHcp) {
-                minHcp = hcp;
-            }
-        }
-        return minHcp == null ? 0 : minHcp;
-    }
-
-    public int minHcp() {
-        Integer minHcp = null;
-        for (Hand h : l) {
-            int hcp = h.numHCP();
-            if (minHcp == null || hcp < minHcp) {
-                minHcp = hcp;
-            }
-        }
-        return minHcp == null ? 0 : minHcp;
-    }
-
-    public int maxHcp() {
-        Integer maxHcp = null;
-        for (Hand h : l) {
-            int hcp = h.numHCP();
-            if (maxHcp == null || hcp > maxHcp) {
-                maxHcp = hcp;
-            }
-        }
-        return maxHcp == null ? 40 : maxHcp;
-    }
-
-    public double avgLenInSuit(int suit) {
-        long sum = 0;
-        for (Hand h : l) {
-            sum += h.numInSuit(suit);
-        }
-        return sum / (double) l.size();
-    }
-
-    public int minInSuit(int suit) {
-        Integer minInSuit = null;
-        for (Hand h : l) {
-            int len = h.numInSuit(suit);
-            if (minInSuit == null || len < minInSuit) {
-                minInSuit = len;
-            }
-        }
-        return minInSuit == null ? 0 : minInSuit;
-    }
-
-    public int maxInSuit(int suit) {
-        Integer maxInSuit = null;
-        for (Hand h : l) {
-            int len = h.numInSuit(suit);
-            if (maxInSuit == null || len > maxInSuit) {
-                maxInSuit = len;
-            }
-        }
-        return maxInSuit == null ? 13 : maxInSuit;
-    }
-
-    @Override
-    public String toString() {
-        return String.valueOf(l);
     }
 }
