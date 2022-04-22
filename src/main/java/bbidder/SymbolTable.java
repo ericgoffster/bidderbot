59dcfc03e7dba76185fdada3116d193b25315603
Eric Goff <egoff@apple.com>
2022-04-21 19:34:48 -0500

    Revert "more basic functionality"
    This reverts commit 6d5fb405c1481cbbf1b84b436a6dfa5b1be6dfb3.

---
+++ src/main/java/bbidder/SymbolTable.java
@@ -0,0 +1,38 @@
+package bbidder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class SymbolTable {
+    public static SymbolTable EMPTY = new SymbolTable(Map.of());
+    private final Map<String, Integer> suits;
+    private SymbolTable(Map<String, Integer> suits) {
+        this.suits = suits;
+    }
+    public SymbolTable add(String symbol, int value) {
+        Map<String, Integer> newSuits = new HashMap<>(suits);
+        newSuits.put(symbol, value);
+        return new SymbolTable(newSuits);
+    }
+    
+    @Override
+    public String toString() {
+        return suits.toString();
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(suits);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SymbolTable other = (SymbolTable) obj;
+        return Objects.equals(suits, other.suits);
+    }
+}
