878fec1291374eb2d9471282c36975cfb6599984
Eric Goff <egoff@apple.com>
2022-04-21 19:34:48 -0500

    Revert "more basic functionality"
    This reverts commit 6d5fb405c1481cbbf1b84b436a6dfa5b1be6dfb3.

--- src/main/java/bbidder/BiddingSystem.java
+++ src/main/java/bbidder/BiddingSystem.java
@@ -165,7 +165,7 @@ public class BiddingSystem {
 
         // Pass means... Nothing else works, this will get smarter.
         if (lastBid == Bid.P) {
-            positiveWild.add(OrBoundInf.create(positive).negate());
+            positiveWild.add(OrBoundInf.create(positiveWild).negate());
             positive.add(OrBoundInf.create(negative).negate());
         }
         if (positive.size() > 0) {
