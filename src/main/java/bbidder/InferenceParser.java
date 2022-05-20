afddb3c3abcb47a8a048fd7468c173faa2592cdb
Eric Goff <egoff@apple.com>
2022-05-20 09:08:41 -0500

    more basic functionality

--- src/main/java/bbidder/InferenceParser.java
+++ src/main/java/bbidder/InferenceParser.java
@@ -1,5 +1,6 @@
 package bbidder;
 
+import java.util.Map;
 import java.util.OptionalInt;
 import java.util.regex.Matcher;
 
@@ -311,7 +312,7 @@ public final class InferenceParser {
             int higher = h.getAsInt();
             return new CombinedTotalPointsRange(PointRange.between(lower, higher));
         }
-        PointRange createRange = CombinedTotalPointsRange.createRange(str);
+        PointRange createRange = InferenceParser.createTPtsRange(str);
         if (createRange == null) {
             return null;
         }
@@ -378,4 +379,50 @@ public final class InferenceParser {
         }
         return null;
     }
+
+    public static PointRange createTPtsRange(String str) {
+        return InferenceParser.createTPtsRange(str, InferenceParser.POINT_RANGES);
+    }
+
+    public static PointRange createTPtsRange(String str, Map<String, Integer> m) {
+        final int dir;
+        if (str.endsWith("+")) {
+            str = str.substring(0, str.length() - 1);
+            dir = 1;
+        } else if (str.endsWith("-")) {
+            str = str.substring(0, str.length() - 1);
+            dir = -1;
+        } else {
+            dir = 0;
+        }
+    
+        if (!m.containsKey(str)) {
+            return null;
+        }
+    
+        Integer pts = m.get(str);
+        if (dir > 0) {
+            return PointRange.between(pts, null);
+        }
+    
+        Integer maxPts = null;
+        for (var e : m.entrySet()) {
+            int nextMax = e.getValue() - 1;
+            if (nextMax >= pts && (maxPts == null || nextMax < maxPts)) {
+                maxPts = nextMax;
+            }
+        }
+    
+        if (dir < 0) {
+            pts = null;
+        }
+    
+        if (maxPts == null) {
+            return PointRange.between(pts, null);
+        }
+    
+        return PointRange.between(pts, maxPts);
+    }
+
+    public static final Map<String, Integer> POINT_RANGES = Map.of("min", 18, "inv", 22, "gf", 25, "slaminv", 31, "slam", 33, "grandinv", 35, "grand", 37);
 }
--- src/main/java/bbidder/inferences/CombinedTotalPointsRange.java
+++ src/main/java/bbidder/inferences/CombinedTotalPointsRange.java
@@ -1,6 +1,5 @@
 package bbidder.inferences;
 
-import java.util.Map;
 import java.util.Objects;
 import java.util.OptionalInt;
 import java.util.stream.Stream;
@@ -20,7 +19,6 @@ import bbidder.inferences.bound.TotalPtsBoundInf;
  *
  */
 public final class CombinedTotalPointsRange extends Inference {
-    private static final Map<String, Integer> STD = Map.of("min", 18, "inv", 22, "gf", 25, "slaminv", 31, "slam", 33, "grandinv", 35, "grand", 37);
     public final PointRange rng;
 
     public CombinedTotalPointsRange(Integer min, Integer max) {
@@ -68,49 +66,5 @@ public final class CombinedTotalPointsRange extends Inference {
             return false;
         CombinedTotalPointsRange other = (CombinedTotalPointsRange) obj;
         return Objects.equals(rng, other.rng);
-    }
-
-    private static PointRange createRange(String str, Map<String, Integer> m) {
-        final int dir;
-        if (str.endsWith("+")) {
-            str = str.substring(0, str.length() - 1);
-            dir = 1;
-        } else if (str.endsWith("-")) {
-            str = str.substring(0, str.length() - 1);
-            dir = -1;
-        } else {
-            dir = 0;
-        }
-
-        if (!m.containsKey(str)) {
-            return null;
-        }
-
-        Integer pts = m.get(str);
-        if (dir > 0) {
-            return PointRange.between(pts, null);
-        }
-
-        Integer maxPts = null;
-        for (var e : m.entrySet()) {
-            int nextMax = e.getValue() - 1;
-            if (nextMax >= pts && (maxPts == null || nextMax < maxPts)) {
-                maxPts = nextMax;
-            }
-        }
-
-        if (dir < 0) {
-            pts = null;
-        }
-
-        if (maxPts == null) {
-            return PointRange.between(pts, null);
-        }
-
-        return PointRange.between(pts, maxPts);
-    }
-
-    public static PointRange createRange(String str) {
-        return createRange(str, STD);
     }
 }
