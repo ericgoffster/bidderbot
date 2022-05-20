cd828819d372d2ae929f474bdb87ad207989afd5
Eric Goff <egoff@apple.com>
2022-05-20 09:08:41 -0500

    more basic functionality

---
+++ src/main/java/bbidder/inferences/SemiBalanced.java
@@ -0,0 +1,56 @@
+package bbidder.inferences;
+
+import java.util.stream.Stream;
+
+import bbidder.IBoundInference;
+import bbidder.Inference;
+import bbidder.Players;
+import bbidder.Shape;
+import bbidder.ShapeSet;
+import bbidder.SuitTable;
+import bbidder.inferences.bound.ShapeBoundInf;
+
+/**
+ * Represents the inference of a "balanced" hand
+ * 
+ * @author goffster
+ *
+ */
+public final class SemiBalanced extends Inference {
+    public static final String NAME = "semibalanced";
+    public static final SemiBalanced SEMI_BALANCED = new SemiBalanced();
+
+    private SemiBalanced() {
+    }
+
+    @Override
+    public IBoundInference bind(Players players) {
+        return ShapeBoundInf.create(ShapeSet.create(Shape::isSemiBalanced));
+    }
+
+    @Override
+    public Stream<Context> resolveSuits(SuitTable suitTable) {
+        return Stream.of(new Context(suitTable));
+    }
+
+    @Override
+    public String toString() {
+        return NAME;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        return true;
+    }
+}
