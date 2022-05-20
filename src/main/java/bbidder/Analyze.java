4e4953e25640075769581de832d83899410d718c
Eric Goff <egoff@apple.com>
2022-05-20 09:08:41 -0500

    more basic functionality

---
+++ src/main/java/bbidder/Analyze.java
@@ -0,0 +1,34 @@
+package bbidder;
+
+import bbidder.utils.DebugUtils;
+
+public class Analyze {
+    public static void showSummary(String label, InfSummary summary) {
+        ShapeStats[] stats = summary.shape.getStats();
+        System.err.println(label);
+        System.err.println("   Spades: " + stats[3]);
+        System.err.println("   Hearts: " + stats[2]);
+        System.err.println("   Diamonds: " + stats[1]);
+        System.err.println("   Clubs: " + stats[0]);
+        System.err.println("   total points: " + summary.tpts);
+        System.err.println("   stoppers: " + summary.stoppers);
+        System.err.println("   partial: " + summary.partialStoppers);
+    }
+
+    public static void main(String[] args) {
+        BiddingSystem bs = BiddingSystemParser.load("classpath:bbo21/index.bidding", ex -> {
+            throw new RuntimeException(ex);
+        });
+        Auction auction = Auction.valueOf(args[0]);
+
+        BiddingState state = new BiddingState(bs);
+        for (Bid bid : auction.getBids()) {
+            DebugUtils.breakpoint();
+            state = state.withBid(bid);
+        }
+        showSummary("me", state.players.rho.infSummary);
+        showSummary("partner", state.players.lho.infSummary);
+        showSummary("lho", state.players.me.infSummary);
+        showSummary("rho", state.players.partner.infSummary);       
+    }
+}
