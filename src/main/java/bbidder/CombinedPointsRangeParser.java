ef51d2b5d6ca111185dde7f1acd99d11d17fc8ba
Eric Goff <egoff@apple.com>
2022-05-20 09:08:41 -0500

    more basic functionality

---
+++ src/main/java/bbidder/CombinedPointsRangeParser.java
@@ -0,0 +1,80 @@
+package bbidder;
+
+import java.util.Map;
+import java.util.OptionalInt;
+
+import bbidder.inferences.CombinedTotalPointsRange;
+import bbidder.utils.SplitUtil;
+
+public class CombinedPointsRangeParser {
+    public static final Map<String, Integer> POINT_RANGES = Map.of("min", 18, "inv", 22, "gf", 25, "slaminv", 31, "slam", 33, "grandinv", 35, "grand",
+            37);
+
+    public static PointRange createCombinedTPtsRange(String str, Map<String, Integer> m) {
+        final int dir;
+        if (str.endsWith("+")) {
+            str = str.substring(0, str.length() - 1);
+            dir = 1;
+        } else if (str.endsWith("-")) {
+            str = str.substring(0, str.length() - 1);
+            dir = -1;
+        } else {
+            dir = 0;
+        }
+
+        if (!m.containsKey(str)) {
+            return null;
+        }
+
+        Integer pts = m.get(str);
+        if (dir > 0) {
+            return PointRange.between(pts, null);
+        }
+
+        Integer maxPts = null;
+        for (var e : m.entrySet()) {
+            int nextMax = e.getValue() - 1;
+            if (nextMax >= pts && (maxPts == null || nextMax < maxPts)) {
+                maxPts = nextMax;
+            }
+        }
+
+        if (dir < 0) {
+            pts = null;
+        }
+
+        if (maxPts == null) {
+            return PointRange.between(pts, null);
+        }
+
+        return PointRange.between(pts, maxPts);
+    }
+
+    public static CombinedTotalPointsRange makeCombinedTPtsRange(String str) {
+        String[] parts = SplitUtil.split(str, "-", 2);
+        if (parts.length == 2 && parts[0].length() > 0 && parts[1].length() > 1) {
+            CombinedTotalPointsRange rlow = makeCombinedTPtsRange(parts[0]);
+            CombinedTotalPointsRange rhigh = makeCombinedTPtsRange(parts[1]);
+            if (rlow == null || rhigh == null) {
+                return null;
+            }
+            OptionalInt l = rlow.rng.lowest();
+            OptionalInt h = rhigh.rng.highest();
+            if (!l.isPresent()) {
+                return new CombinedTotalPointsRange(PointRange.NONE);
+            }
+            if (!h.isPresent()) {
+                return new CombinedTotalPointsRange(PointRange.NONE);
+            }
+            int lower = l.getAsInt();
+            int higher = h.getAsInt();
+            return new CombinedTotalPointsRange(PointRange.between(lower, higher));
+        }
+        PointRange createRange = createCombinedTPtsRange(str, CombinedPointsRangeParser.POINT_RANGES);
+        if (createRange == null) {
+            return null;
+        }
+        return new CombinedTotalPointsRange(createRange);
+    }
+
+}
--- src/main/java/bbidder/InferenceParser.java
+++ src/main/java/bbidder/InferenceParser.java
@@ -1,14 +1,11 @@
 package bbidder;
 
-import java.util.Map;
-import java.util.OptionalInt;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import bbidder.inferences.Always;
 import bbidder.inferences.AndInference;
 import bbidder.inferences.Balanced;
-import bbidder.inferences.CombinedTotalPointsRange;
 import bbidder.inferences.FitInSuit;
 import bbidder.inferences.HCPRange;
 import bbidder.inferences.LongestOrEqual;
@@ -35,8 +32,6 @@ import bbidder.utils.SplitUtil;
  *
  */
 public final class InferenceParser {
-    private static final Map<String, Integer> POINT_RANGES = Map.of("min", 18, "inv", 22, "gf", 25, "slaminv", 31, "slam", 33, "grandinv", 35,
-            "grand", 37);
     private static Pattern PATT_SPECIFIC_CARDS = Pattern.compile("of\\s+top\\s+(\\d+)\\s+in\\s+(.*)", Pattern.CASE_INSENSITIVE);
     /**
      * @param str
@@ -196,7 +191,7 @@ public final class InferenceParser {
                 }
             }
             {
-                Inference i = makeCombinedTPtsRange(str.trim());
+                Inference i = CombinedPointsRangeParser.makeCombinedTPtsRange(str.trim());
                 if (i != null) {
                     return i;
                 }
@@ -205,72 +200,5 @@ public final class InferenceParser {
         }
         }
         throw new IllegalArgumentException("unknown inference: '" + str + "'");
-    }
-
-    private static CombinedTotalPointsRange makeCombinedTPtsRange(String str) {
-        String[] parts = SplitUtil.split(str, "-", 2);
-        if (parts.length == 2 && parts[0].length() > 0 && parts[1].length() > 1) {
-            CombinedTotalPointsRange rlow = makeCombinedTPtsRange(parts[0]);
-            CombinedTotalPointsRange rhigh = makeCombinedTPtsRange(parts[1]);
-            if (rlow == null || rhigh == null) {
-                return null;
-            }
-            OptionalInt l = rlow.rng.lowest();
-            OptionalInt h = rhigh.rng.highest();
-            if (!l.isPresent()) {
-                return new CombinedTotalPointsRange(PointRange.NONE);
-            }
-            if (!h.isPresent()) {
-                return new CombinedTotalPointsRange(PointRange.NONE);
-            }
-            int lower = l.getAsInt();
-            int higher = h.getAsInt();
-            return new CombinedTotalPointsRange(PointRange.between(lower, higher));
-        }
-        PointRange createRange = createCombinedTPtsRange(str, POINT_RANGES);
-        if (createRange == null) {
-            return null;
-        }
-        return new CombinedTotalPointsRange(createRange);
-    }
-
-    private static PointRange createCombinedTPtsRange(String str, Map<String, Integer> m) {
-        final int dir;
-        if (str.endsWith("+")) {
-            str = str.substring(0, str.length() - 1);
-            dir = 1;
-        } else if (str.endsWith("-")) {
-            str = str.substring(0, str.length() - 1);
-            dir = -1;
-        } else {
-            dir = 0;
-        }
-
-        if (!m.containsKey(str)) {
-            return null;
-        }
-
-        Integer pts = m.get(str);
-        if (dir > 0) {
-            return PointRange.between(pts, null);
-        }
-
-        Integer maxPts = null;
-        for (var e : m.entrySet()) {
-            int nextMax = e.getValue() - 1;
-            if (nextMax >= pts && (maxPts == null || nextMax < maxPts)) {
-                maxPts = nextMax;
-            }
-        }
-
-        if (dir < 0) {
-            pts = null;
-        }
-
-        if (maxPts == null) {
-            return PointRange.between(pts, null);
-        }
-
-        return PointRange.between(pts, maxPts);
     }
 }
