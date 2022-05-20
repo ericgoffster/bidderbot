b298c7681a5fb8a7e5206a7f6270e4bd44739bf3
Eric Goff <egoff@apple.com>
2022-05-20 09:08:41 -0500

    more basic functionality

---
+++ src/main/java/bbidder/symbols/SeatSymbol.java
@@ -0,0 +1,67 @@
+package bbidder.symbols;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import bbidder.Bid;
+import bbidder.Symbol;
+import bbidder.SuitTable;
+
+public final class SeatSymbol extends Symbol {
+    public static final String NAME = "seat";
+    private final Symbol symbol;
+    private final short seats;
+
+    public SeatSymbol(Symbol symbol, short seats) {
+        super();
+        this.symbol = symbol;
+        this.seats = seats;
+    }
+
+    @Override
+    public String toString() {
+        return symbol + ":" + NAME + seats;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(seats, symbol);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SeatSymbol other = (SeatSymbol) obj;
+        return seats == other.seats && Objects.equals(symbol, other.symbol);
+    }
+
+    @Override
+    public Stream<Context> resolveSuits(SuitTable suitTable) {
+        return symbol.resolveSuits(suitTable).map(e -> new SeatSymbol(e.getSymbol(), seats).new Context(e.suitTable));
+    }
+
+    @Override
+    public int getResolvedStrain() {
+        return symbol.getResolvedStrain();
+    }
+
+    @Override
+    public boolean compatibleWith(Bid bid) {
+        return symbol.compatibleWith(bid);
+    }
+    
+    @Override
+    public short getSeats() {
+        return seats;
+    }
+
+    @Override
+    public boolean isNonConvential() {
+        return false;
+    }
+}
